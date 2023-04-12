c8781ed1b3fe449339c620e8d30d0b7bc1c53b2a
s24468 <s24468@pjwstk.edu.pl>
2023-04-12 14:31:51 +0200

    są dobre wartości, brzydkie wykresy

--- PSM_5/MainWindow.xaml.cs
+++ PSM_5/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ namespace PSM_5
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int NUMSTEPS = 400;
+
         public MainWindow()
         {
             // Stałe
@@ -49,7 +51,7 @@ namespace PSM_5
                 DVyM;
 
             // Przykładowa liczba kroków
-            int numSteps = 1000;
+            int numSteps = NUMSTEPS;
 
             // Współrzędne początkowe
             double Xe = 0, Ye = 0;
@@ -58,8 +60,8 @@ namespace PSM_5
             VxM = Vm;
             VyM = 0;
             InitializeComponent();
-            var chart = new ChartValues<ObservablePoint>()
-                { };
+            var chart = new ChartValues<ObservablePoint>();
+            var chart2 = new ChartValues<ObservablePoint>();
             var list = ZiemiaList();
             int i = 0;
             for (int t = 0; t < numSteps; t++)
@@ -90,20 +92,29 @@ namespace PSM_5
                 VxM += DVxM;
                 VyM += DVyM;
                 chart.Add(new ObservablePoint(Xm + list[i], Ym + list[i + 1]));
+                chart2.Add(new ObservablePoint(list[i], list[i + 1]));
+                // chart.Add(new ObservablePoint(list[i], list[i + 1]));
                 i += 2;
                 // Wypisanie wyników dla kroku t (opcjonalne)
                 // Console.WriteLine($"Krok {t}: Xm={Xm}, Ym={Ym}, VxM={VxM}, VyM={VyM}");
             }
 
 
-            var line = new LineSeries()
+            var ksiezycSeries = new LineSeries()
             {
-                Title = "Jarek",
+                Title = "KSIEZYC",
+                Values = chart,
+                // PointGeometry = null // Usuwa punkty z wykresu
+            };
+            var ziemiaSeries = new LineSeries()
+            {
+                Title = "ZIEMIA",
                 Values = chart,
                 // PointGeometry = null // Usuwa punkty z wykresu
             };
             var seria = new SeriesCollection();
-            seria.Add(line);
+            seria.Add(ksiezycSeries);
+            seria.Add(ziemiaSeries);
             var cartesianChart = new CartesianChart()
             {
                 Series = seria,
@@ -111,82 +122,70 @@ namespace PSM_5
             Content = cartesianChart;
         }
 
+
         private static List<double> ZiemiaList()
         {
+            List<double> list = new List<double>();
             double G = 6.6743e-11;
             double Ms = 1.989e+30;
             double Me = 5.972e+24;
             double Des = 1.5e+11;
             double dt = 21600;
+            double Xs = 0;
+            double Ys = 0;
             double Ve = 29749.15427;
 
-            // Zmienne
-            double Xs,
-                Ys,
-                VxS,
-                VyS,
-                F,
-                FxS,
-                FyS,
-                AxS,
-                AyS,
-                Xs_2,
-                Ys_2,
-                Fx_2,
-                Fy_2,
-                VxS_2,
-                VyS_2,
-                AxS_2,
-                AyS_2,
-                DxS,
-                DyS,
-                DVxS,
-                DVyS;
+            // Inicjalizacja zmiennych
+            double Xe = Xs;
+            double Ye = Ys + Des;
+            double VxE = Ve;
+            double VyE = 0;
+            double F, FxE, FyE, AxE, AyE, Xe_2, Ye_2, Fx_2, Fy_2, VxE_2, VyE_2, AxE_2, AyE_2, DxE, DyE, DVxE, DVyE;
 
-            // Przykładowa liczba kroków
-            int numSteps = 1000;
+            int steps = NUMSTEPS; // liczba kroków czasowych (można dostosować)
 
-            // Współrzędne początkowe
-            double Xe = 0, Ye = 0;
-            Xs = Xe;
-            Ys = Ye + Des;
-            VxS = Ve;
-            VyS = 0;
-            List<double> list = new List<double>();
-
-            for (int t = 0; t < numSteps; t++)
+            for (int t = 0; t < steps; t++)
             {
                 // Obliczenia dla kroku t
-                Des = Math.Sqrt(Math.Pow(Xs - Xe, 2) + Math.Pow(Ys - Ye, 2));
+                Des = Math.Sqrt(Math.Pow(Xe - Xs, 2) + Math.Pow(Ye - Ys, 2));
                 F = G * Ms * Me / Math.Pow(Des, 2);
-                FxS = (Xs - Xe) / Des * F;
-                FyS = (Ys - Ye) / Des * F;
-                AxS = FxS / Me;
-                AyS = FyS / Me;
-                Xs_2 = Xs + VxS * dt / 2;
-                Ys_2 = Ys + VyS * dt / 2;
-                Fx_2 = (Xs_2 - Xe) / Des * F;
-                Fy_2 = (Ys_2 - Ye) / Des * F;
-                VxS_2 = VxS + AxS * dt / 2;
-                VyS_2 = VyS + AyS * dt / 2;
-                AxS_2 = Fx_2 / Me;
-                AyS_2 = Fy_2 / Me;
-                DxS = VxS_2 * dt;
-                DyS = VyS_2 * dt;
-                DVxS = AxS_2 * dt;
-                DVyS = AyS_2 * dt;
+                FxE = (Xs - Xe) / Des * F;
+                FyE = (Ys - Ye) / Des * F;
+                AxE = FxE / Me;
+                AyE = FyE / Me;
+                Xe_2 = Xe + VxE * dt / 2;
+                Ye_2 = Ye + VyE * dt / 2;
+                Fx_2 = (Xs - Xe_2) / Des * F;
+                Fy_2 = (Ys - Ye_2) / Des * F;
+                VxE_2 = VxE + AxE * dt / 2;
+                VyE_2 = VyE + AyE * dt / 2;
+                AxE_2 = Fx_2 / Me;
+                AyE_2 = Fy_2 / Me;
+                DxE = VxE_2 * dt;
+                DyE = VyE_2 * dt;
+                DVxE = AxE_2 * dt;
+                DVyE = AyE_2 * dt;
 
                 // Aktualizacja zmiennych
-                Xs += DxS;
-                Ys += DyS;
-                VxS += DVxS;
-                VyS += DVyS;
+                Xe += DxE;
+                Ye += DyE;
+                VxE += DVxE;
+                VyE += DVyE;
 
-                list.Add(Xe);
-                list.Add(Ye);
-                // Wypisanie wyników dla kroku t (opcjonalne)
-                // Console.WriteLine($"Krok {t}: Xs={Xs}, Ys={Ys}, VxS={VxS}, VyS={VyS}");
+                // Wyświetlanie wyników
+                Console.WriteLine($"Krok {t + 1}:");
+                Console.WriteLine($"Xe: {Xe}");
+                Console.WriteLine($"Ye: {Ye}");
+                Console.WriteLine($"VxE: {VxE}");
+                Console.WriteLine($"DVyE: {DVyE}");
+                Console.WriteLine($"AyE_2: {AyE_2}");
+                Console.WriteLine($"Fy_2: {Fy_2}");
+
+
+                list.Add(Xs / 10);
+                list.Add(Ys / 10);
             }
+
 
             return list;
         }
