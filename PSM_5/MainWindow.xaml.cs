4c7497981e4784b9648d8dd130e056e4e0320aa4
s24468 <s24468@pjwstk.edu.pl>
2023-04-12 21:26:24 +0200

    czysty piękny kod

--- PSM_5/MainWindow.xaml.cs
+++ PSM_5/MainWindow.xaml.cs
@@ -11,74 +11,60 @@ namespace PSM_5
     /// </summary>
     public partial class MainWindow
     {
-        const int Numsteps = 365 * 3; // 3 lata
-        const double Dt = 86400; //jeden dzien
+        private const int Numsteps = 365 * 3; // 3 lata
+        private const double Dt = 86400; //jeden dzien
         private const double GravityConstant = 6.6743e-11;
-        const double SunMass = 1.989e+30;
-        const double EarthMass = 5.972e+24;
-        const double MoonMass = 7.347e+22;
+        private const double SunMass = 1.989e+30;
+        private const double EarthMass = 5.972e+24;
+        private const double MoonMass = 7.347e+22;
+        private const double XCoordinatesSun = 0;
+        private const double YCoordinatesSun = 0;
 
         public MainWindow()
         {
             // Stałe
-            double DistanceEarthSun = 1.5e+11;
-            double DistanceEarthMoon = 384400000;
-            double velocityEarth = 29749.15427;
-            double velocityMoon = 1018.289046;
-
-            // Zmienne
-            double xAccelerationMoon, yAccelerationMoon;
-            double forceGravity, xForceGravity, yForceGravity;
-            double Xm_2, Ym_2;
-            double Fx_2, Fy_2;
-            double VxM_2, VyM_2;
-            double AxM_2, AyM_2;
-            double DxM, DyM;
-            double DVxM, DVyM;
-
-            // liczba kroków
-            int numSteps = Numsteps;
+            double distanceEarthMoon = 384400000;
+            const double velocityMoon = 1018.289046;
 
             // Współrzędne początkowe
-            double xCoordinatesEarth = 0, yCoordinatesEarth = 0;
+            const double xCoordinatesEarth = 0;
+            const double yCoordinatesEarth = 0;
             var xCoordinatesMoon = xCoordinatesEarth;
-            var yCoordinatesMoon = yCoordinatesEarth + DistanceEarthMoon;
+            var yCoordinatesMoon = yCoordinatesEarth + distanceEarthMoon;
             var xVelocityMoon = velocityMoon;
             double yVelocityMoon = 0;
-            
-            
+
+
             InitializeComponent();
             var chart = new ChartValues<ObservablePoint>();
             var list = EarthList();
-            int i = 0;
-            for (int t = 0; t < numSteps; t++)
+            var i = 0;
+            for (var t = 0; t < Numsteps; t++)
             {
                 // Obliczenia dla kroku t
-                DistanceEarthMoon = Math.Sqrt(Math.Pow(xCoordinatesEarth - xCoordinatesMoon, 2) +
+                distanceEarthMoon = Math.Sqrt(Math.Pow(xCoordinatesEarth - xCoordinatesMoon, 2) +
                                               Math.Pow(yCoordinatesEarth - yCoordinatesMoon, 2));
-                forceGravity = GravityConstant * EarthMass * MoonMass / Math.Pow(DistanceEarthMoon, 2);
-                xForceGravity = (xCoordinatesEarth - xCoordinatesMoon) / DistanceEarthMoon * forceGravity;
-                yForceGravity = (yCoordinatesEarth - yCoordinatesMoon) / DistanceEarthMoon * forceGravity;
-                xAccelerationMoon = xForceGravity / MoonMass;
-                yAccelerationMoon = yForceGravity / MoonMass;
-                Xm_2 = xCoordinatesMoon + xVelocityMoon * Dt / 2;
-                Ym_2 = yCoordinatesMoon + yVelocityMoon * Dt / 2;
-                Fx_2 = (xCoordinatesEarth - Xm_2) / DistanceEarthMoon * forceGravity;
-                Fy_2 = (yCoordinatesEarth - Ym_2) / DistanceEarthMoon * forceGravity;
-                VxM_2 = xVelocityMoon + xAccelerationMoon * Dt / 2;
-                VyM_2 = yVelocityMoon + yAccelerationMoon * Dt / 2;
-                AxM_2 = Fx_2 / MoonMass;
-                AyM_2 = Fy_2 / MoonMass;
-                DxM = VxM_2 * Dt;
-                DyM = VyM_2 * Dt;
-                DVxM = AxM_2 * Dt;
-                DVyM = AyM_2 * Dt;
+                var forceGravity = GravityConstant * EarthMass * MoonMass / Math.Pow(distanceEarthMoon, 2);
+                var xForceGravity = (xCoordinatesEarth - xCoordinatesMoon) / distanceEarthMoon * forceGravity;
+                var yForceGravity = (yCoordinatesEarth - yCoordinatesMoon) / distanceEarthMoon * forceGravity;
+                var xAccelerationMoon = xForceGravity / MoonMass;
+                var yAccelerationMoon = yForceGravity / MoonMass;
+                var xMoonMidPointCoordinates = xCoordinatesMoon + xVelocityMoon * Dt / 2; //yEarthMidPointCoordinates
+                var yMoonMidPointCoordinates = yCoordinatesMoon + yVelocityMoon * Dt / 2; //yEarthMidPointCoordinates
+                var xMoonMidPointForce =
+                    (xCoordinatesEarth - xMoonMidPointCoordinates) / distanceEarthMoon * forceGravity; //
+                var yMoonMidPointForce =
+                    (yCoordinatesEarth - yMoonMidPointCoordinates) / distanceEarthMoon * forceGravity; //
+                var xMoonMidPointVelocity = xVelocityMoon + xAccelerationMoon * Dt / 2; //yMoonMidPointVelocity
+                var yMoonMidPointVelocity = yVelocityMoon + yAccelerationMoon * Dt / 2; //yMoonMidPointVelocity
+                var xMoonMidPointAcceleration = xMoonMidPointForce / MoonMass; //
+                var yMoonMidPointAcceleration = yMoonMidPointForce / MoonMass; //
 
                 // Aktualizacja zmiennych
-                xCoordinatesMoon += DxM;
-                yCoordinatesMoon += DyM;
-                xVelocityMoon += DVxM;
-                yVelocityMoon += DVyM;
+                xCoordinatesMoon += xMoonMidPointVelocity * Dt;
+                yCoordinatesMoon += yMoonMidPointVelocity * Dt;
+                xVelocityMoon += xMoonMidPointAcceleration * Dt;
+                yVelocityMoon += yMoonMidPointAcceleration * Dt;
                 chart.Add(new ObservablePoint(xCoordinatesMoon + list[i], yCoordinatesMoon + list[i + 1]));
                 i += 2;
             }
@@ -90,11 +76,10 @@ namespace PSM_5
                 Values = chart,
                 PointGeometry = null // Usuwa punkty z wykresu
             };
-            var seria = new SeriesCollection();
-            seria.Add(moonSeries);
+            var serial = new SeriesCollection() { moonSeries };
             var cartesianChart = new CartesianChart()
             {
-                Series = seria,
+                Series = serial,
             };
             Content = cartesianChart;
         }
@@ -104,56 +89,46 @@ namespace PSM_5
         {
             List<double> list = new List<double>();
 
-            double distanceEarthSun = 1.5e+11;
-            const double xCoordinatesSun = 0;
-            const double yCoordinatesSun = 0;
-            double velocityEarth = 29749.15427;
+            var distanceEarthSun = 1.5e+11;
+            var velocityEarth = 29749.15427;
 
             // Inicjalizacja zmiennych
-            double xCoordinatesEarth = xCoordinatesSun;
-            double yCoordinatesEarth = yCoordinatesSun + distanceEarthSun;
-            double xVelocityEarth = velocityEarth;
+            var xCoordinatesEarth = XCoordinatesSun;
+            var yCoordinatesEarth = YCoordinatesSun + distanceEarthSun;
+            var xVelocityEarth = velocityEarth;
             double yVelocityEarth = 0;
 
-
-            double Xe_2, Ye_2, Fx_2, Fy_2, VxE_2, VyE_2, AxE_2, AyE_2, DxE, DyE, DVxE, DVyE;
-
-            int steps = Numsteps; // liczba kroków czasowych (można dostosować)
-
-            for (int t = 0; t < steps; t++)//coordinates
+            for (var t = 0; t < Numsteps; t++) //coordinates
             {
                 // Obliczenia dla kroku t
-                distanceEarthSun = Math.Sqrt(Math.Pow(xCoordinatesEarth - xCoordinatesSun, 2) +
-                                             Math.Pow(yCoordinatesEarth - yCoordinatesSun, 2));
+                distanceEarthSun = Math.Sqrt(Math.Pow(xCoordinatesEarth - XCoordinatesSun, 2) +
+                                             Math.Pow(yCoordinatesEarth - YCoordinatesSun, 2));
                 var forceGravity = GravityConstant * SunMass * EarthMass / Math.Pow(distanceEarthSun, 2);
-                var xForceGravity = (xCoordinatesSun - xCoordinatesEarth) / distanceEarthSun * forceGravity;
-                var yForceGravity = (yCoordinatesSun - yCoordinatesEarth) / distanceEarthSun * forceGravity;
+                var xForceGravity = (XCoordinatesSun - xCoordinatesEarth) / distanceEarthSun * forceGravity;
+                var yForceGravity = (YCoordinatesSun - yCoordinatesEarth) / distanceEarthSun * forceGravity;
                 var xAccelerationEarth = xForceGravity / EarthMass;
                 var yAccelerationEarth = yForceGravity / EarthMass;
-                Xe_2 = xCoordinatesEarth + xVelocityEarth * Dt / 2;
-                Ye_2 = yCoordinatesEarth + yVelocityEarth * Dt / 2;
-                Fx_2 = (xCoordinatesSun - Xe_2) / distanceEarthSun * forceGravity;
-                Fy_2 = (yCoordinatesSun - Ye_2) / distanceEarthSun * forceGravity;
-                VxE_2 = xVelocityEarth + xAccelerationEarth * Dt / 2;
-                VyE_2 = yVelocityEarth + yAccelerationEarth * Dt / 2;
-                AxE_2 = Fx_2 / EarthMass;
-                AyE_2 = Fy_2 / EarthMass;
-                DxE = VxE_2 * Dt;
-                DyE = VyE_2 * Dt;
-                DVxE = AxE_2 * Dt;
-                DVyE = AyE_2 * Dt;
+                var xEarthMidPointCoordinates = xCoordinatesEarth + xVelocityEarth * Dt / 2;
+                var yEarthMidPointCoordinates = yCoordinatesEarth + yVelocityEarth * Dt / 2;
+                var xEarthMidPointForce =
+                    (XCoordinatesSun - xEarthMidPointCoordinates) / distanceEarthSun * forceGravity;
+                var yEarthMidPointForce =
+                    (YCoordinatesSun - yEarthMidPointCoordinates) / distanceEarthSun * forceGravity;
+                var xEarthMidPointVelocity = xVelocityEarth + xAccelerationEarth * Dt / 2;
+                var yEarthMidPointVelocity = yVelocityEarth + yAccelerationEarth * Dt / 2;
+                var xEarthMidPointAcceleration = xEarthMidPointForce / EarthMass;
+                var yEarthMidPointAcceleration = yEarthMidPointForce / EarthMass;
 
                 // Aktualizacja zmiennych
-                xCoordinatesEarth += DxE;
-                yCoordinatesEarth += DyE;
-                xVelocityEarth += DVxE;
-                yVelocityEarth += DVyE;
+                xCoordinatesEarth += xEarthMidPointVelocity * Dt;
+                yCoordinatesEarth += yEarthMidPointVelocity * Dt;
+                xVelocityEarth += xEarthMidPointAcceleration * Dt;
+                yVelocityEarth += yEarthMidPointAcceleration * Dt;
 
 
                 list.Add(xCoordinatesEarth / 10);
                 list.Add(yCoordinatesEarth / 10);
             }
-
 
             return list;
         }
